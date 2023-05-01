3d88b44ed8c4e661f7fbee677a85f737ccdfc08b
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-01 19:46:05 -0300

    feat: document and app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,58 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { Inter } from 'next/font/google'
+import logoRsxp from '@/assets/logo.png'
+
+import { StarFour } from '@phosphor-icons/react'
+import { GithubUserProvider } from '@/contexts/GithubUserContext';
+
+const inter = Inter({
+  subsets: ['latin']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GithubUserProvider>
+      <div className={`max-w-[1920px] mx-auto h-screen grid grid-rows-[85px_9fr_2fr] overflow-hidden relative ${inter.className}`}>
+        <header className='px-6 py-3'>
+          <Image src={logoRsxp} width={170} height={60} alt="logo rocketseat experience" priority className='object-cover' />
+        </header>
+
+        <Component {...pageProps} />
+
+        <footer className='flex flex-col gap-4 mb-4 absolute bottom-0 left-0'>
+          <motion.ul className="h-8 bg-rocketseat-light list-none flex gap-2 items-center whitespace-nowrap" whileHover={{ scale: 1.1 }}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+              <motion.li
+                key={item}
+                initial={{ translateX: '-100%' }}
+                animate={{ x: '100%' }}
+                transition={{ duration: 5, ease: 'linear', repeat: Infinity, repeatType: 'loop' }}
+                className='uppercase flex-shrink-0 font-medium flex gap-2 items-center'
+              >
+                30 de Junho e 01 de Julho <StarFour />
+              </motion.li>
+            ))}
+          </motion.ul>
+
+          <motion.ul className="h-8 bg-new-light list-none flex gap-2 items-center whitespace-nowrap" whileHover={{ scale: 1.1 }}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+              <motion.li
+                key={item}
+                animate={{ x: '-100%' }}
+                transition={{ duration: 5, ease: 'linear', repeat: Infinity, repeatType: 'loop' }}
+                className='uppercase flex-shrink-0 font-medium flex gap-2 items-center'
+              >
+                Expo Center Norte - SP <StarFour />
+              </motion.li>
+            ))}
+          </motion.ul>
+        </footer>
+      </div>
+    </GithubUserProvider>
+  )
 }
