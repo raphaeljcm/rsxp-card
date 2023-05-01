bd07b4353c6b254de7c0936f9f73b5155fc8af1e
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-01 19:46:16 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,118 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { GitHubUser } from "@/@types/types";
+import { useGithubUser } from "@/contexts/GithubUserContext";
+import { ArrowRight, CircleNotch, Check, X } from "@phosphor-icons/react";
+import axios from "axios";
+import { AnimatePresence, motion } from 'framer-motion';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { KeyboardEvent, useRef, useState } from "react";
 
 export default function Home() {
+  const [showInput, setShowInput] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [fetchFinished, setFetchFinished] = useState(false);
+  const [error, setError] = useState(false);
+
+  const { addUser } = useGithubUser();
+  const router = useRouter();
+
+  const usernameRef = useRef<HTMLInputElement>(null)
+
+  const handleFetchGithubData = async () => {
+    const username = usernameRef.current?.value;
+    if (!username) return;
+
+    try {
+      setLoading(true);
+      setError(false);
+
+      const { data } = await axios.get<GitHubUser>(`${process.env.NEXT_PUBLIC_GITHUB_API_URL}/users/${username}`);
+
+      const githubUser = {
+        name: data.name,
+        avatar_url: data.avatar_url,
+        login: data.login,
+        bio: data.bio,
+      }
+
+      // mocking a request delay
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      addUser(githubUser);
+
+      setLoading(false);
+      setFetchFinished(true);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      router.push('/badge');
+    } catch (err) {
+      setLoading(false);
+      setError(true);
+    }
+  }
+
+  const handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && handleFetchGithubData();
+
+  const handleButtonKeyDown = (e: KeyboardEvent<HTMLDivElement>) => e.key === 'Enter' && setShowInput(true);
+
+  const handleButtonIcon = () => {
+    if (error) return <X color="red" size={18} />;
+    if (!loading && !fetchFinished) return <ArrowRight color="white" size={18} />;
+    if (loading && !fetchFinished) return <CircleNotch color="white" size={18} className="animate-spin" />;
+    if (!loading && fetchFinished) return <Check color="white" size={18} />
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Head >
+        <title>RX/XP | Home</title>
+      </Head>
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className="flex flex-col items-center text-center gap-2 mt-60"
+      >
+        <p className="uppercase text-zinc-300 font-medium">Crie seu card para o rs/xp</p>
+        <h1 className="text-transparent bg-heading bg-clip-text font-bold text-3xl leading-[1.2] max-w-xl">
+          Vamos para o maior evento presencial da Rocketseat?
+        </h1>
+        <AnimatePresence>
+          {!showInput && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={() => setShowInput(true)}
+              onKeyDown={handleButtonKeyDown}
+              className="p-[1px] bg-gradient-to-tr from-rocketseat-light to-rocketseat-dark rounded-md mt-5"
+            >
+              <button
+                type="button"
+                className="text-zinc-200 h-12 px-12 rounded-md font-medium bg-black shadow-button hover:bg-gradient-to-r hover:from-rocketseat-light hover:via-rocketseat-dark hover:to-rocketseat-low hover:text-black transition-colors duration-300">
+                Criar card
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <AnimatePresence>
+          {showInput && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.5, duration: 0.5 }}
+              className="relative p-[1px] bg-gradient-to-tr from-gray-500 to-gray-700 rounded-md focus-within:from-rocketseat-light focus-within:to-rocketseat-dark     transition-colors duration-300 mt-5">
+              <input placeholder="Digite o seu github username" ref={usernameRef} onKeyDown={handleInputKeyDown} className="text-zinc-200 text-sm h-12 p-4 w-72 outline-none rounded-md font-medium bg-black transition-colors duration-500" />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <button onClick={handleFetchGithubData} className="bg-gray-500/50 absolute right-2 top-1/4 rounded-full p-1">
+                {handleButtonIcon()}
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.main>
+    </>
   )
 }
