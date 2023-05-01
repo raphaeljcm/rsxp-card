92d55f78d947729d3e2023b754d10a1496c45181
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-01 19:42:27 -0300

    feat: creates GithubUserContext

---
+++ src/contexts/GithubUserContext.tsx
@@ -0,0 +1,28 @@
+import { GitHubUser } from "@/@types/types";
+import { ReactNode, createContext, useCallback, useContext, useState } from "react";
+
+interface GithubUserData {
+  user: GitHubUser;
+  addUser: (user: GitHubUser) => void;
+}
+
+interface GithubUserProviderProps {
+  children: ReactNode;
+}
+
+
+const GithubUserContext = createContext({} as GithubUserData)
+
+export function GithubUserProvider({ children }: GithubUserProviderProps) {
+  const [user, setUser] = useState({} as GitHubUser);
+
+  const addUser = useCallback((user: GitHubUser) => setUser(user), []);
+
+  return (
+    <GithubUserContext.Provider value={{ user, addUser }}>
+      {children}
+    </GithubUserContext.Provider>
+  )
+}
+
+export const useGithubUser = () => useContext(GithubUserContext);
