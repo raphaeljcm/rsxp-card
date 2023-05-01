99704ab9a4a6385a8835b13e004fd6537ef797a3
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-01 19:40:53 -0300

    feat: creates tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
   ],
   theme: {
     extend: {
+      colors: {
+        rocketseat: {
+          light: '#996DFF',
+          mid: '#8257E5',
+          dark: '#633BBC',
+          low: '#271A45',
+        },
+        success: {
+          light: '#04D361',
+          base: '#1B873F',
+          low: '#051B0D',
+        },
+        danger: {
+          light: '#F75A68',
+          base: '#CC2937',
+          low: '#2D090C',
+        },
+        new: {
+          light: '#1EF7D0',
+          base: '#07847E',
+          low: '#163840',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          400: '#7C7C8A',
+          500: '#505059',
+          600: '#323238',
+          700: '#29292E',
+          800: '#202024',
+          900: '#121214',
+          950: '#09090A',
+        },
+      },
+      boxShadow: {
+        button: 'rgba(153, 109, 255, 0.15) 0px 4px 5px 3px',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        heading: 'linear-gradient(to right bottom, rgb(255, 255, 255) 30%, rgba(255, 255, 255, 0.38))',
       },
     },
   },
