ea706fd15a6592c8281c84917e24f3ca989e6c7f
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-02 17:59:03 -0300

    chore: creates og route and enable animation

---
+++ src/pages/api/og.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  return response.status(200).json({ message: "I'm here" });
+}
