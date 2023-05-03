92224016b522895c5ef3d5d589317731fe2cf59b
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-03 09:48:28 -0300

    feat: adding open graph tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,13 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          property="og:title"
+          content="Crie seu card para o maior evento presencial da Rocketseat"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://rsxp-card.vercel.app" />
+        <meta property="og:image" content="/rocketseat-og.png" />
       </Head>
       <body>
         <Main />
