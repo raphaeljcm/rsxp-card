38d5ff9075c48b9380153493f2d02f0670638a7f
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-03 10:39:39 -0300

    feat: add twitter og tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta
+          name="description"
+          content="Crie seu card e mostre para o mundo que você irá no RS/XP 2023. Uma experiência única, imersiva e transformadora de 2 dias em São Paulo/SP com palestras, conteúdo, muito código, networking e atividades pra você acelerar sua evolução na programação."
+        />
+        <meta
           property="og:title"
           content="Crie seu card para o maior evento presencial da Rocketseat"
         />
@@ -16,23 +20,18 @@ export default function Document() {
           property="og:description"
           content="Crie seu card e mostre para o mundo que você irá no RS/XP 2023."
         />
-        <meta
-          name="description"
-          content="Crie seu card e mostre para o mundo que você irá no RS/XP 2023. Uma experiência única, imersiva e transformadora de 2 dias em São Paulo/SP com palestras, conteúdo, muito código, networking e atividades pra você acelerar sua evolução na programação."
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="rsxp-card.vercel.app" />
         <meta property="twitter:url" content="https://rsxp-card.vercel.app" />
         <meta
-          name="twitter:title"
+          property="twitter:title"
           content="Crie seu card para o maior evento presencial da Rocketseat"
         />
         <meta
-          name="twitter:description"
+          property="twitter:description"
           content="Crie seu card e mostre para o mundo que você irá no RS/XP 2023."
         />
-        <meta name="twitter:image" content="/rocketseat-og.png" />
+        <meta property="twitter:image" content="/rocketseat-og.png" />
       </Head>
       <body>
         <Main />
