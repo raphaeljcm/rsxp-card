967b9e1c7b2a4dee3a8e83357db5d60e7eedfab2
Raphael Marques <raphaeljcmarques@gmail.com>
2023-05-02 23:30:16 -0300

    refactor: remove toLowerCase fn

--- src/pages/api/auth/linkedin/callback.ts
+++ src/pages/api/auth/linkedin/callback.ts
@@ -4,5 +4,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  return res.status(200).json({ message: 'Hello World' });
+  return res.status(200).json({ message: 'Hello World.' });
 }
